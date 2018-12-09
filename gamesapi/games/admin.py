380be99610ba706d9a6c22a0ae52f28e2ef35a3b
Milad Hatami <miladhzz@gmail.com>
2018-12-09 07:22:54 +0330

    Working with Hyperlinked APIs
    from rest framework, url is TEMP

--- gamesapi/games/admin.py
+++ gamesapi/games/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Game
+from .models import Game, GameCategory, Player, PlayerScore
 
 # Register your models here.
 admin.site.register(Game)
+admin.site.register(GameCategory)
+admin.site.register(Player)
+admin.site.register(PlayerScore)
